d453c07fbb64112ab33101fbb81be36743fabe31
William <williamsalimrjs@gmail.com>
2025-03-10 20:40:21 +0700

    hai

--- database/seeders/RolePermissionSeeder.php
+++ database/seeders/RolePermissionSeeder.php
@@ -24,21 +24,26 @@ class RolePermissionSeeder extends Seeder
         $this->createPermissions();
 
         $rootPermissions = [
-            'read-dashboard', 'access-project-page', 'access-project-page', 'access-account-page', 'access-master-page'
+            'read-dashboard', 'access-project-page', 'access-account-page', 'access-master-page'
         ];
 
         $ownerPermission = [
-            'read-dashboard', 'access-project-page', 'access-project-page', 'access-account-page'
+            'read-dashboard', 'access-project-page', 'access-account-page'
         ];
 
         $consultanPermissions = [
             'read-dashboard'
         ];
 
+        $talentPermissions = [
+            'access-talent'
+        ];
+
         # Assigning Permission
         $roles['root']->givePermissionTo($rootPermissions);
         $roles['owner']->givePermissionTo($ownerPermission);
         $roles['consultant']->givePermissionTo($consultanPermissions);
+        $roles['talent']->givePermissionTo($talentPermissions);
     }
 
     private function createRoles()
@@ -56,6 +61,10 @@ class RolePermissionSeeder extends Seeder
                 'name' => 'consultant',
                 'guard_name' => 'api'
             ]),
+            'talent' => Role::create([
+                'name' => 'talent',
+                'guard_name' => 'api'
+            ]),
         ];
     }
 
@@ -66,6 +75,7 @@ class RolePermissionSeeder extends Seeder
             ['name' => 'access-project-page', 'guard_name' => 'api'],
             ['name' => 'access-master-page', 'guard_name' => 'api'],
             ['name' => 'access-account-page', 'guard_name' => 'api'],
+            ['name' => 'access-talent', 'guard_name' => 'api'],
         ]);
     }
 }
